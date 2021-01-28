6783435fa9a601b8869b3ece505c65acd987c474
Corbeau01 <corvus-corax@hotmail.com>
2021-01-28 11:31:15 -0500

    ColliderFix

---
+++ Assets/DesactivateColliders.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DesactivateColliders : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < this.transform.childCount; i++)
+        {
+            for (int j = 0; j < 2; j++)
+            {
+                if (this.transform.GetChild(i).transform.GetChild(j).GetComponent<Collider>())
+                {
+                    this.transform.GetChild(i).transform.GetChild(j).GetComponent<Collider>().isTrigger = true;
+                }
+            }
+            
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
