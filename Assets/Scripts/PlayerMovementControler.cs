c8befcfa62844577b4bee67a6409c7111b4e0d84
BrOverShield <tmdudydude@hotmail.com>
2021-01-29 00:49:05 -0500

    new player controller

---
+++ Assets/Scripts/PlayerMovementControler.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovementControler : MonoBehaviour
+{
+    float x;
+    float y;
+    Rigidbody rb;
+    public float speed = 10.0f;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        x = 0.0f;
+        y = 0.0f;
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        x = Input.GetAxis("Horizontal");
+        y = Input.GetAxis("Vertical");
+
+        rb.velocity = transform.forward * y*speed+transform.right*x*speed;
+    }
+}
